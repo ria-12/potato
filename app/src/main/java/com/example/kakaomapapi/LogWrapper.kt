b5e8ca69c1788cb975bc1ffbba818224a43d7551
godjoy <tlschgml556@gmail.com>
2020-07-02 17:41:03 +0900

    kakao map setup

---
+++ app/src/main/java/com/example/kakaomapapi/LogWrapper.kt
@@ -0,0 +1,11 @@
+package com.example.kakaomapapi
+
+import java.io.BufferedWriter
+import java.io.File
+import java.io.FileWriter
+import java.io.IOException
+import java.util.logging.Logger
+
+class LogWrapper {
+    
+}
